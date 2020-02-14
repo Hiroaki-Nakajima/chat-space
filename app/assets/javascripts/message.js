6c091f746dc8d874f39300927656828741e93850
Hiroaki-Nakajima <h.19860820@gmail.com>
2020-02-14 20:54:26 +0900

    自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,56 @@
 $(function(){
-  function buildHTML(message){
-    if ( message.image ) {
-      var html =
-       `<div class="message-box" data-message-id=${message.id}>
-          <div class="message-box__message-title">
-            <div class="message-box__message-title__message-name">
-              ${message.user_name}
-            </div>
-            <div class="message-box__message-title__message-date">
-              ${message.created_at}
-            </div>
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      var html = 
+      `<div class="message-box" data-message-id=${message.id}>
+        <div class="message-box__message-title">
+          <div class="message-box__message-title__message-name">
+            ${message.user_name}
           </div>
-          <div class="message-box__message-main">
+          <div class="message-box__message-title__message-date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-box__message-main">
             <p class="message-box__message-main__content">
               ${message.content}
             </p>
+        </div>
+        <img src=${message.image} class="message-box__message-main__image" >
+      </div>`
+    } else if (message.content) {
+      var html = 
+      `<div class="message-box" data-message-id=${message.id}>
+        <div class="message-box__message-title">
+          <div class="message-box__message-title__message-name">
+            ${message.user_name}
           </div>
-          <img src=${message.image} class="message-box__message-main__image" >
-        </div>`
-      return html;
-    } else {
-      var html =
-       `<div class="message-box" data-message-id=${message.id}>
-          <div class="message-box__message-title">
-            <div class="message-box__message-title__message-name">
-              ${message.user_name}
-            </div>
-            <div class="message-box__message-title__message-date">
-              ${message.created_at}
-            </div>
+          <div class="message-box__message-title__message-date">
+            ${message.created_at}
           </div>
-          <div class="message-box__message-main">
+        </div>
+        <div class="message-box__message-main">
             <p class="message-box__message-main__content">
               ${message.content}
             </p>
+        </div>
+      </div>`
+    } else if (message.image) {
+      var html = 
+      `<div class="message-box" data-message-id=${message.id}>
+        <div class="message-box__message-title">
+          <div class="message-box__message-title__message-name">
+            ${message.user_name}
           </div>
-        </div>`
-      return html;
+          <div class="message-box__message-title__message-date">
+            ${message.created_at}
+          </div>
+        </div>
+        <img src=${message.image} class="message-box__message-main__image" >
+      </div>`
     };
-  }
+    return html;
+  };
 
   $('#new_message').on('submit', function(e){
     e.preventDefault()
@@ -64,4 +76,28 @@ $(function(){
       $('.new-message__send-btn').prop("disabled", false);
     });
   })
+
+  var reloadMessages = function() {
+    last_message_id = $('.message-list:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.message-list').append(insertHTML);
+        $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      }
+    })
+  }
+
+  if (document.location.href.match(/\/groups\/\d+\/message/)) {
+    setInterval(reloadMessages, 7000);
+  }
 })
